40f0cf32582c6bfb5ce982b6aed9491285bd7a13
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-06 16:55:13 +0200

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 export function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <h1>Portfolio website</h1>
+
+            <div id='one'></div>
+            <div id='two'></div>
+            <div id='three'></div>
+        </>
+    )
 }
