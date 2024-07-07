0c6b1bf41fca3346329edb0a23b03b4be83a9077
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-07 22:51:15 +0200

    add hero section to home page

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import {Section} from "./Section.tsx";
+import {Heading} from "./Heading.tsx";
+import {LinkButton} from "./LinkButton.tsx";
+import styles from './Hero.module.scss'
+import stylesButtons from './LinkButton.module.scss'
+
+export const Hero = () => {
+    return <Section className={styles.container}>
+        <Heading title={"Hello, I'm Sasha"} subtitle={'Junior full-stack web developer with graphic design background'}
+                 extra={<>
+                     <LinkButton to={'https://github.com/oleksandralaskaj'}>Github</LinkButton>
+                     <LinkButton to='/' hash="#contact" className={stylesButtons.button}>Contact</LinkButton>
+                     <LinkButton to="/files/valko_oleksandra_cv_en_cz_developer.pdf" download={true}>Download
+                         CV</LinkButton>
+                 </>
+                 }/>
+    </Section>
+}
