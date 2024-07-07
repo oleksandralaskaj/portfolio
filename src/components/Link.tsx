ed14e2dc406bbc19ef35c3d0aae15b8671031353
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-07 22:49:57 +0200

    add link reusable component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,18 @@
 import {Link as ReactRouterLink, To} from "react-router-dom";
 import {PropsWithChildren} from "react";
+import {HashLink} from "react-router-hash-link";
+import styles from './Link.module.scss'
 
 type Props = PropsWithChildren<{
-    to: To
+    to: To;
+    hash?: boolean
 }>
 
-export const Link = ({to, children}: Props) => {
-    return <ReactRouterLink to={to}>{children}</ReactRouterLink>
+export const Link = ({to, children, hash}: Props) => {
+    return <div>
+        {hash ?
+            <HashLink to={`${to}${hash}`} className={styles.link}>{children}</HashLink> :
+            <ReactRouterLink to={to} download={download} target='_blank'
+                             className={styles.link}>{children}</ReactRouterLink>}
+    </div>
 }
