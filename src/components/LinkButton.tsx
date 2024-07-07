4d17dfd054f9370df104602df7bdbc8f55757f7b
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-07 22:50:12 +0200

    add link-button reusable component

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,9 +1,17 @@
-import {Link, To} from "react-router-dom";
+import {Link as ReactRouterLink, To} from "react-router-dom";
 import {PropsWithChildren} from "react";
+import {HashLink} from "react-router-hash-link";
+import styles from './LinkButton.module.scss'
 
 type Props = PropsWithChildren<{
-    to: To
+    to: To,
+    download?: boolean,
+    hash?: string
 }>
-export const LinkButton = ({children, to}: Props) => {
-    return <Link to={to}>{children}</Link>
+export const LinkButton = ({children, to, download, hash}: Props) => {
+    return <div className={styles.button}>
+        {hash ?
+            <HashLink to={`${to}${hash}`}>{children}</HashLink> :
+            <ReactRouterLink to={to} download={download} target='_blank'>{children}</ReactRouterLink>}
+    </div>
 }
