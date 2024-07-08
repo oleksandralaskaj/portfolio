fd82b053b3addd1f660b9d10c9f3a61d238e2b58
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-08 14:25:48 +0200

    add icons to the buttons in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,21 @@ import {Section} from "./Section.tsx";
 import {Heading} from "./Heading.tsx";
 import {LinkButton} from "./LinkButton.tsx";
 import styles from './Hero.module.scss'
-import {ArrowDownToLine} from "lucide-react";
+import {ArrowDownToLine, AtSign, MoveUpRight} from "lucide-react";
 
 export const Hero = () => {
     return <Section className={styles.container}>
         <Heading title={"Hello, I'm Sasha"} subtitle={'Junior full-stack web developer with graphic design background'}
                  extra={<>
-                     <LinkButton to={'https://github.com/oleksandralaskaj'}>Github</LinkButton>
-                     <LinkButton to='/' hash="#contact">Contact</LinkButton>
-                     <LinkButton to="/files/valko_oleksandra_cv_en_cz_developer.pdf" download={true}><ArrowDownToLine /> Download
-                         CV</LinkButton>
+                     <LinkButton to={'https://github.com/oleksandralaskaj'}>
+                         <MoveUpRight/>Github
+                     </LinkButton>
+                     <LinkButton to='/' hash="#contact">
+                         <AtSign/>Contact
+                     </LinkButton>
+                     <LinkButton to="/files/valko_oleksandra_cv_en_cz_developer.pdf" download={true}>
+                         <ArrowDownToLine/> Download CV
+                     </LinkButton>
                  </>
                  }/>
     </Section>
