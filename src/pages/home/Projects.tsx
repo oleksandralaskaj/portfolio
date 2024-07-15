c65d2f5902e43ef84b7de52f23fd29a5cca2d3f8
oleksandralaskaj <alexandravalko@gmail.com>
2024-07-15 09:34:48 +0200

    changed gaps on main page

--- src/pages/home/Projects.tsx
+++ src/pages/home/Projects.tsx
@@ -28,128 +28,130 @@ export const Projects = () => {
           </>
         }
       />
-      <ProjectPreview
-        title={"Floor planner app"}
-        subtitle={
-          <>
-            WORK IN PROGRESS: an app created for
-            <Nbsp />
-            real estate agents and home owner, that
-            <Nbsp />
-            seek accurate representation of
-            <Nbsp />
-            property without unneeded technicalities.
-          </>
-        }
-        imgBigUrl={"/images/floor_planner_title.png"}
-        imgSmallUrl={"/images/floor_planner_secondary.png"}
-        btns={
-          <>
-            <LinkButton
-              to={
-                "https://github.com/oleksandralaskaj/web_dev_bootcamp/tree/main/FLOOR_PLANNER_APP"
-              }
-            >
-              <MoveUpRight />
-              Github
-            </LinkButton>
-            {/*<LinkButton to={""}>*/}
-            {/*  <MoveUpRight />*/}
-            {/*  Real thing*/}
-            {/*</LinkButton>*/}
-            {/*<LinkButton to={''}>*/}
-            {/*    <MoveUpRight/>Figma Project*/}
-            {/*</LinkButton>*/}
-          </>
-        }
-        stack={
-          <>
-            <img src="/images/logo-react.svg" alt="react-logo" />
-            <img src="/images/logo-typescript.svg" alt="typescript-logo" />
-            <img src="/images/logo-laravel.svg" alt="laravel-logo" />
-            {/*<img src="/images/logo-figma.svg" alt="figma-logo"/>*/}
-          </>
-        }
-      />
-      <ProjectPreview
-        title={"My portfolio"}
-        subtitle={
-          "One place with detailed information about my background and recent projects."
-        }
-        imgBigUrl={"/images/my_portfolio_title.jpg"}
-        imgSmallUrl={"/images/my_portfolio_secondary.png"}
-        btns={
-          <>
-            <TransitiveLink to={"/my-portfolio"}>
-              <Button>
+      <div className={styles.content}>
+        <ProjectPreview
+          title={"Floor planner app"}
+          subtitle={
+            <>
+              WORK IN PROGRESS: an app created for
+              <Nbsp />
+              real estate agents and home owner, that
+              <Nbsp />
+              seek accurate representation of
+              <Nbsp />
+              property without unneeded technicalities.
+            </>
+          }
+          imgBigUrl={"/images/floor_planner_title.png"}
+          imgSmallUrl={"/images/floor_planner_secondary.png"}
+          btns={
+            <>
+              <LinkButton
+                to={
+                  "https://github.com/oleksandralaskaj/web_dev_bootcamp/tree/main/FLOOR_PLANNER_APP"
+                }
+              >
                 <MoveUpRight />
-                <p>More info</p>
-              </Button>
-            </TransitiveLink>
-            <LinkButton to={"https://github.com/oleksandralaskaj/portfolio"}>
-              <MoveUpRight />
-              Github
-            </LinkButton>
-            <LinkButton
-              to={
-                "https://www.figma.com/proto/kwmqoJ6YDZgIByMfGiBeHw/Portfolio?node-id=1-2&t=qBMIfWxTLxf3nXO7-1"
-              }
-            >
-              <MoveUpRight />
-              Figma Project
-            </LinkButton>
-          </>
-        }
-        stack={
-          <>
-            <img src="/images/logo-react.svg" alt="react-logo" />
-            <img src="/images/logo-typescript.svg" alt="typescript-logo" />
-            <img src="/images/logo-figma.svg" alt="figma-logo" />
-          </>
-        }
-        to={"/my-portfolio"}
-        reverse
-      />
-      <ProjectPreview
-        title={"Label beauty website"}
-        subtitle={
-          <>
-            A platform shared by
-            <Nbsp />
-            beauty salon and skincare e-shop under umbrella of
-            <Nbsp />
-            Label Beauty. Concept, web is
-            <Nbsp />
-            currently under development.
-          </>
-        }
-        imgBigUrl={"/images/label_beauty_title.png"}
-        imgSmallUrl={"/images/label_beauty_mood.jpg"}
-        btns={
-          <>
-            <TransitiveLink to={"/label-beauty"}>
-              <Button>
+                Github
+              </LinkButton>
+              {/*<LinkButton to={""}>*/}
+              {/*  <MoveUpRight />*/}
+              {/*  Real thing*/}
+              {/*</LinkButton>*/}
+              {/*<LinkButton to={''}>*/}
+              {/*    <MoveUpRight/>Figma Project*/}
+              {/*</LinkButton>*/}
+            </>
+          }
+          stack={
+            <>
+              <img src="/images/logo-react.svg" alt="react-logo" />
+              <img src="/images/logo-typescript.svg" alt="typescript-logo" />
+              <img src="/images/logo-laravel.svg" alt="laravel-logo" />
+              {/*<img src="/images/logo-figma.svg" alt="figma-logo"/>*/}
+            </>
+          }
+        />
+        <ProjectPreview
+          title={"My portfolio"}
+          subtitle={
+            "One place with detailed information about my background and recent projects."
+          }
+          imgBigUrl={"/images/my_portfolio_title.jpg"}
+          imgSmallUrl={"/images/my_portfolio_secondary.png"}
+          btns={
+            <>
+              <TransitiveLink to={"/my-portfolio"}>
+                <Button>
+                  <MoveUpRight />
+                  <p>More info</p>
+                </Button>
+              </TransitiveLink>
+              <LinkButton to={"https://github.com/oleksandralaskaj/portfolio"}>
                 <MoveUpRight />
-                <p>More info</p>
-              </Button>
-            </TransitiveLink>
-            <LinkButton
-              to={
-                "https://www.figma.com/proto/QeqPBGaeX92Arg8sIlDwgE/labelbeauty.cz?node-id=0-1&t=tQIcCxQRM3bZEj8Q-1"
-              }
-            >
-              <MoveUpRight />
-              Figma Project
-            </LinkButton>
-          </>
-        }
-        stack={
-          <>
-            <img src="/images/logo-figma.svg" alt="figma-logo" />
-          </>
-        }
-        to={"/label-beauty"}
-      />
+                Github
+              </LinkButton>
+              <LinkButton
+                to={
+                  "https://www.figma.com/proto/kwmqoJ6YDZgIByMfGiBeHw/Portfolio?node-id=1-2&t=qBMIfWxTLxf3nXO7-1"
+                }
+              >
+                <MoveUpRight />
+                Figma Project
+              </LinkButton>
+            </>
+          }
+          stack={
+            <>
+              <img src="/images/logo-react.svg" alt="react-logo" />
+              <img src="/images/logo-typescript.svg" alt="typescript-logo" />
+              <img src="/images/logo-figma.svg" alt="figma-logo" />
+            </>
+          }
+          to={"/my-portfolio"}
+          reverse
+        />
+        <ProjectPreview
+          title={"Label beauty website"}
+          subtitle={
+            <>
+              A platform shared by
+              <Nbsp />
+              beauty salon and skincare e-shop under umbrella of
+              <Nbsp />
+              Label Beauty. Concept, web is
+              <Nbsp />
+              currently under development.
+            </>
+          }
+          imgBigUrl={"/images/label_beauty_title.png"}
+          imgSmallUrl={"/images/label_beauty_mood.jpg"}
+          btns={
+            <>
+              <TransitiveLink to={"/label-beauty"}>
+                <Button>
+                  <MoveUpRight />
+                  <p>More info</p>
+                </Button>
+              </TransitiveLink>
+              <LinkButton
+                to={
+                  "https://www.figma.com/proto/QeqPBGaeX92Arg8sIlDwgE/labelbeauty.cz?node-id=0-1&t=tQIcCxQRM3bZEj8Q-1"
+                }
+              >
+                <MoveUpRight />
+                Figma Project
+              </LinkButton>
+            </>
+          }
+          stack={
+            <>
+              <img src="/images/logo-figma.svg" alt="figma-logo" />
+            </>
+          }
+          to={"/label-beauty"}
+        />
+      </div>
     </Section>
   );
 };
